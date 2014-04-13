0da920087d95ff9cd4cc5b2084627642692eadb5
Ahmed Farooki <ahmedfarooki@gmail.com>
2014-04-13 11:54:00 -0700

    Added login.php script

---
+++ login.php
@@ -0,0 +1,41 @@
+<?php
+    function checkUserCred($user, $pass) {
+        if ($user === "ammaar" && $pass === "admin") {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+
+    if (isset($_POST['login'] && isset($_POST['password'])) {
+        $userPermissions = checkUserCred($_POST['login'], $_POST['password']);
+    }
+
+    if ($userPermissions) {
+?>
+    Hello Ammaaar!
+
+<?php
+    }
+    else {
+?>
+
+<h2>Login form</h2>
+<form action="login.php" method="post">
+<table border="1">
+<tr>
+  <td>Login</td>
+  <td><input type="text" name="login" /></td>
+</tr>
+<tr>
+  <td>Password</td>
+  <td><input type="password" name="password" /></td>
+</tr>
+</table>
+</form>
+
+<?php
+    }
+?>
