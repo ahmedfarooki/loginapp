fcbdc30bab8fbe78819e77537896d8f0c826a769
Ahmed Farooki <ahmedfarooki@gmail.com>
2014-04-13 12:14:27 -0700

    Forgot to add a login button

--- login.php
+++ login.php
@@ -33,6 +33,10 @@
   <td>Password</td>
   <td><input type="password" name="password" /></td>
 </tr>
+<tr>
+  <td></td>
+  <td><input type="submit" name="login" value="Login" /></td>
+</tr>
 </table>
 </form>
 
